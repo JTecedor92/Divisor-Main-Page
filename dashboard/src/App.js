bc7fb9e3422aa1c1f467742602921781f94261ae
Jorge Tecedor <jtecedor292@gmail.com>
2024-07-09 20:28:46 -0400

    Co-authored-by: teddyfoster08@gmail.com <teddyfoster08@gmail.com> Co-authored-by: GideonDayo <GideonDayo@users.noreply.github.com>

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import Main from './components/Main.js'
 import LoadingScreen from './components/LoadingScreen.js'
+import Editor from './components/Editor.js'
 import {useState} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
 
   const [elementvisible,setVisible] = useState('Main');
 
   return (
-    <div className="App">
-      <div style={{display: elementvisible==='Loading' ? 'inline-block' : 'none' , width: '100%' , height: '100%'}}>
-        <LoadingScreen/>
-      </div>
-      <div style={{display: elementvisible==='Main' ? '' : 'none', width: '100%' , height: '100%'}}>
-        <Main/>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/main' Component={Main}/>
+        <Route path='/loading' Component={LoadingScreen}/>
+        <Route path='/editor' Component={Editor}/>
+      </Routes>
+    </Router>
   );
 }
 
